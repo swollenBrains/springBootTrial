7e8697a87ff9b2a88af830ccd76a6c77de3b3662
ankitgupta7 <ankit.gupta7@cognizant.com>
2016-04-21 11:26:21 +0000

    Adding trial controller to spring boot application

---
+++ src/main/java/trial/springboot/Application.java
@@ -0,0 +1,14 @@
+package trial.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+		System.out.println("Hit http://localhost:8080/healthcheck to check if its working");
+	}
+	
+}
